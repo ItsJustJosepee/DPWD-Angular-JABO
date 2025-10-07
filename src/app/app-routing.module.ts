0e97f788c9ec35aee35a27786a38b364bf733608
5L1 <5L1@DESKTOP-BCRSKIR>
2025-10-07 08:33:25 -0600

    AVANCE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// Asegúrate de que todos estos componentes estén importados correctamente
-import { PlantillaComponent } from './plantilla/plantilla.component';  // Verifica la ruta del componente
-import { ValidacionesComponent } from './validaciones/validaciones.component'; // Verifica la ruta del componente
-import { FormulariosReactivosComponent } from './formularios-reactivos/formularios-reactivos.component'; // Verifica la ruta del componente
-import { PadreComponent } from './padre/padre.component'; // Verifica la ruta del componente
-import { HijoComponent } from './hijo/hijo.component'; // Verifica la ruta del componente
-import { Padre2Component } from './padre2/padre2.component'; // Verifica la ruta del componente
-import { Hijo2Component } from './hijo2/hijo2.component'; // Verifica la ruta del componente
+import { PlantillaComponent } from './plantilla/plantilla.component'; 
+import { ValidacionesComponent } from './validaciones/validaciones.component';
+import { FormulariosReactivosComponent } from './formularios-reactivos/formularios-reactivos.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
+import { Padre2Component } from './padre2/padre2.component';
+import { Hijo2Component } from './hijo2/hijo2.component';
 
 const routes: Routes = [
   { path: 'Formulario', component: PlantillaComponent },
@@ -18,8 +17,8 @@ const routes: Routes = [
   { path: 'hijo', component: HijoComponent },
   { path: 'padre2', component: Padre2Component },
   { path: 'hijo2', component: Hijo2Component },
-  { path: '', redirectTo: '/Formulario', pathMatch: 'full' }, // Redirige al componente 'Formulario' al inicio
-  { path: '**', redirectTo: '/Formulario' } // Redirige a 'Formulario' si no existe la ruta
+  { path: '', redirectTo: '/Formulario', pathMatch: 'full' },
+  { path: '**', redirectTo: '/Formulario' } 
 ];
 
 @NgModule({
