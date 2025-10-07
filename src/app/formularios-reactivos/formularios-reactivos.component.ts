7cf520ebfae753445b3c9b192d87c277c557f190
José Arath <josbaort24@outlook.es>
2025-10-07 15:25:18 -0600

    Avance 2

--- src/app/formularios-reactivos/formularios-reactivos.component.ts
+++ src/app/formularios-reactivos/formularios-reactivos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formularios-reactivos',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './formularios-reactivos.component.css'
 })
 export class FormulariosReactivosComponent {
+  registroForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      fecha: ['', [Validators.required, Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]]
+    });
+  }
+  onSubmit() {
+    if (this.registroForm && this.registroForm.valid) {
+      console.log('Formulario válido. Datos;');
+      console.log('Nombre:', this.registroForm.value.nombre);
+      console.log('Correo Electrónico:', this.registroForm.value.email);
+      console.log('Contraseña:', this.registroForm.value.password);
+      this.MostrarDatos = true;
+    } else {
+      console.log('Formulario inválido. Por favor, complete todos los campos correctamente.');
+    }
+  }
 }
