173e3fabe3e4b584cf5c0a96e30f4c1edb6228c0
5L1 <5L1@DESKTOP-BCRSKIR>
2025-10-14 08:30:38 -0600

    Vercel Analiticas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+// importa todo el módulo de analytics
+import * as vercelAnalytics from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TerceraEtapaJABO';
+export class AppComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Verifica que esté corriendo en el navegador (no en Node)
+    if (typeof window !== 'undefined') {
+      // Inicializa analytics
+      vercelAnalytics.inject();
+
+      // Trackea navegación interna
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          vercelAnalytics.inject();
+          console.log('📊 Vercel Analytics: ruta visitada →', event.urlAfterRedirects);
+        }
+      });
+    }
+  }
 }
