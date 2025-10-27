f8571943aa72abc9a6bd9fdd76bef1933522838e
5L1 <itsjustjosepee@users.noreply.github.com>
2025-10-27 07:48:23 -0600

    TODO GG

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css'
+  styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  @ViewChild('carouselTrack', { static: false }) track!: ElementRef;
+  currentIndex = 0;
 
+  nextSlide() {
+    const track = document.querySelector('.carousel-track') as HTMLElement;
+    const total = track.children.length;
+    this.currentIndex = (this.currentIndex + 1) % total;
+    track.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+  }
+
+  prevSlide() {
+    const track = document.querySelector('.carousel-track') as HTMLElement;
+    const total = track.children.length;
+    this.currentIndex = (this.currentIndex - 1 + total) % total;
+    track.style.transform = `translateX(-${this.currentIndex * 100}%)`;
+  }
 }
